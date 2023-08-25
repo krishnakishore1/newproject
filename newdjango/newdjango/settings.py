451aebcd6246493b03a5e587dfcd0a29cbbdc3e7
krishnakishore1 <117765184+krishnakishore1@users.noreply.github.com>
2023-08-25 21:14:41 +0530

    Update settings.py

--- newdjango/newdjango/settings.py
+++ newdjango/newdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-anh*23@gr%s&xp7o=5@!3ej%jkvyavz)#ls%2jwhq%4#xl!6#v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['65.0.29.223','localhost','0.0.0.0']
+ALLOWED_HOSTS = ['15.206.173.35','localhost','0.0.0.0']
 
 
 # Application definition
